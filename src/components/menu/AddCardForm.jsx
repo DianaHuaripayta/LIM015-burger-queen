7700cc133c17aaff8cb510baf7625fc9d77eb228
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-10-26 15:43:13 -0500

    add alerts in crud

--- src/components/menu/AddCardForm.jsx
+++ src/components/menu/AddCardForm.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable array-callback-return */
 import { FaPlus, FaMinus, FaRegTrashAlt} from "react-icons/fa";
 import './table.css'
-export function AddCardForm({ card, setCard }) {//cart y setCard con que contiene los cards que le dieron click
-    const total = card.reduce((acc, item) => acc + item.quantity * item.price , 0)//item el elemento actual | 0 porque comienza a multiplicar de 0
-    /* const arrayObjCard = card; */
+export function AddCardForm({ card, setCard, buttonActive, setbuttonActive}) {//cart y setCard con que contiene los cards que le dieron click
+    const total = card.reduce((acc, item) => acc + item.quantity * item.price , 0)//item el elemento actual | 0 
+    
     const fnResta = (id) => {
         card.map((product) => {
             if (product.id === id && product.quantity > 1) {
+                
                 setCard(
                     card.map((products) =>
                       products.id === product.id ? 
@@ -22,20 +23,23 @@ export function AddCardForm({ card, setCard }) {//cart y setCard con que contien
         card.map((product) => {
             if (product.id === id) {
                 setCard(
-                    card.map((products) =>
-                      products.id === product.id ? 
-                      { ...products, 
-                        quantity: products.quantity + 1 
-                      } : products)
+                card.map((products) =>
+                    products.id === product.id ? 
+                    { ...products, 
+                    quantity: products.quantity + 1 
+                    } : products)
                 );         
-            }
-           
+            }  
         })
     }
-
+    
+    const deleteProduct = (id) => {
+        setCard(card.filter(item => item.id !== id))
+        
+        setbuttonActive(true)
+    }
     return (    
-            <>
-            <div className="table-responsive">
+            <><div className="table-responsive">
             <table className="table table-borderless">
                 <thead className="table-active">
                     <tr>{/* className="table-primary" */}
@@ -47,35 +51,41 @@ export function AddCardForm({ card, setCard }) {//cart y setCard con que contien
                 </thead>
                 <tbody>
                     {card.length === 0 ? (<tr>
-                            <td>Agregar</td><td>Agregar</td><td>Agregar</td>
-                        </tr>
+                        <td>Agregar</td><td>Agregar</td><td>Agregar</td>
+                    </tr>
                     ) : (card.map((product) => (
                         <tr>
                             <td>{product.name} </td>
                             <td>s/ {product.price}</td>
                             <td>
-                            <FaMinus style={{color: "#78c2ad", fontSize: '15px', margin:'6px'}}  onClick={() => fnResta(product.id)}/> {product.quantity}
-                            
-                            <FaPlus style={{color: "#78c2ad", fontSize: '15px', margin:'6px'}} onClick={() => fnSuma(product.id)}/>
-                            
-                            <FaRegTrashAlt style={{color: "#78c2ad", fontSize: '15px', margin:'6px' , cursor:'pointer'}} onClick={()=> setCard(card.filter(item => item.id !== product.id))}/>
+                                <FaMinus style={{ color: "#78c2ad", fontSize: '15px', margin: '6px' }} onClick={() => fnResta(product.id)} /> {product.quantity}
+
+                                <FaPlus style={{ color: "#78c2ad", fontSize: '15px', margin: '6px' }} onClick={() => fnSuma(product.id)} />
+
+                                <FaRegTrashAlt style={{ color: "#78c2ad", fontSize: '15px', margin: '6px', cursor: 'pointer' }}  disabled={buttonActive} onClick={() => deleteProduct(product.id)} />
                             </td>
                         </tr>
+
+
                     ))
 
                     )}
                 </tbody>
                 <tfoot className="table-active">
                     <tr>
-                        <td colSpan="2"><strong>Total</strong></td>{/*colSpan 'S' Uppercase */}
-                        <td className="text-right"><strong>{total}.00</strong></td>
+                        <td colSpan="2">
+                            <strong>Total</strong>
+                        </td>
+                        <td>
+                            <strong>{total}.00</strong>
+                        </td>
+
                     </tr>
+
                 </tfoot>
             </table>
         </div>
-
-        
-         
-            </>                      
+        </>                
     )
 };
+/* () => setCard(card.filter(item => item.id !== product.id)) */
