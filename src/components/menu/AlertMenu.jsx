94a00afc104e7ab1e691a789e55a34162c67f83f
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-10-26 20:54:41 -0500

    add delete all products

--- src/components/menu/AlertMenu.jsx
+++
@@ -1,9 +0,0 @@
-export function AlertMenu() {
-    return (
-        <div>
-        <div className="alert alert-success" role="alert">
-        A simple success alert—check it out!
-        </div>
-        </div>
-    )
-}
--- src/components/menu/ProductsCard.jsx
+++ src/components/menu/ProductsCard.jsx
@@ -1,12 +1,12 @@
 import './styleCards.css'
-import React, {useState} from "react"
+import React from "react"
 import swal from 'sweetalert';
 //AGREGAR CARTAS AL CONTENEDOR PADRE
 export function ProductsCard({ product,productMenu, card, setCard}) {
     const { id, name, price, img } = product; //product es cada obj que contiene
     /* state button */
-    const [buttonActive, setbuttonActive] = useState(false);
-
+    /* const [buttonActive, setbuttonActive] = useState(false); */
+    /* disabled={buttonActive}-- en el button */
     const addInList = () =>{
         const existProduct = card.find((products) => products.id === product.id)
         if (existProduct) {
@@ -17,15 +17,16 @@ export function ProductsCard({ product,productMenu, card, setCard}) {
                   quantity: existProduct.quantity + 1 
                 } : products)
                 );   
-                 
+                
           } else {
             setCard([...card, { ...product, quantity: 1 }]);  
+            
             swal({
               title: "Agregado",
               button:"Aceptar",
               timer:"2000"
             });    
-            setbuttonActive(true)
+            /* setbuttonActive(true) */
                 
           }
         
@@ -41,7 +42,7 @@ export function ProductsCard({ product,productMenu, card, setCard}) {
                     {productMenu ? ((<section>s/ {price}</section>)): (<></>)}{/* Si el product menu ya esta, se agregara section pero sino se suma y solo se agrega un fragment */}
                     <h5 className="card-title designName">{name}</h5>
                     {productMenu ? ((
-                      <button className='btn btn-primary btn-block' disabled={buttonActive} onClick={() => addInList(id)}>Agregar</button>
+                      <button className='btn btn-primary btn-block' onClick={() => addInList(id)}>Agregar</button>
                     ))
                     : (<></>)}
                 </div>
