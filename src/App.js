035b8014f8b2ef9e6b1f070dec2562c7ff8567c5
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-10-28 20:45:38 -0500

    add new files and images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Menu } from "./components/menu/Menu.jsx";
-
+import { Login } from "./components/login/Login.jsx";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-      <Menu/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/menu" component={Menu}></Route>
+       
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/menu/InputName.jsx
@@ -0,0 +1,17 @@
+
+/* import React, { useRef } from 'react';
+export function InputName() {
+    const inputName = useRef();
+    return (
+        <div>
+             <div className="form-group">
+                <div className="form-group">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text"></span>
+                        <input type="text" className="form-control" ref={inputName} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} */
